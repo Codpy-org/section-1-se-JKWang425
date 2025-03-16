8077669b51a6400e67d4e013c54fa694c989d31e
JACK <wjulie0425@gmail.com>
2025-03-16 14:55:52 +0800

    fest: unit test

--- UnitTestHW/Regular Expression Matching/test_solution.py
+++ UnitTestHW/Regular Expression Matching/test_solution.py
@@ -1,8 +1,12 @@
-import ...
-from ...
+import pytest
+from solution import pytest
 
 @pytest.fixture
-...
+def solution():
+    class Wrapper:
+        def isMatch(self, s, p):
+            return isMatch(s, p)
+    return Wrapper()
     
 testcases = [
     # s, p, expected_res
@@ -19,8 +23,7 @@ testcases = [
 ...
     assert solution.isMatch(s, p) == expected
 
-"""
+
 @pytest.mark.xfail
 def test_broken_solution(solution):
     assert solution.isMatch() == 
-"""
